fe74147344f0ff06443a0be006ab7f8aa28722da
ooooo5h <ooooo5h91@gmail.com>
2021-12-23 14:41:54 +0900

    컴퓨터 문제 출제 로직
    random.sample 기능 활용

--- 03_repeat_test_hard_08.py
+++ 03_repeat_test_hard_08.py
@@ -1,10 +1,11 @@
-# 컴퓨터가 랜덤으로 3자리 숫자를 출력하자
+import random
 
-cpu_numbers = []
+# 컴퓨터가 랜덤으로 3자리 숫자를 출력하자
 
 # 3개의 숫자를 추가해주자
 # 랜덤 1 ~ 9 + 중복은 허용하지 X
 
+cpu_numbers = random.sample( range (1, 10), 3 ) 
 
 # 테스트 : 출제되는 숫자들이 뭔지 확인해보자
 print(cpu_numbers)
