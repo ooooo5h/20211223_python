c0a915b62bf289b2cd44da9079ca1383d2db00de
ooooo5h <ooooo5h91@gmail.com>
2021-12-23 10:42:11 +0900

    검사1 추가 : 1~45 이내 숫자만 리스트에 담기기
    해당 숫자가 아니면 리스트에 안담김

--- 02_repeat_test_hard_07.py
+++ 02_repeat_test_hard_07.py
@@ -8,8 +8,11 @@ my_num_list = []
 for i in range(6):
     input_num = int(input(f'{i+1}번째 숫자 입력 : '))
     
-    # 입력받은 숫자를 목록에 추가
-    my_num_list.append(input_num)
+    # 받은 숫자가 목록에 추가해도 되는 숫자인가? 검사해보고 => 통과하면 추가해주는걸로 변경
+    # 검사 1 : 1~45 이내의 숫자?
+    if 1 <= input_num <= 45:
+        # 입력받은 숫자를 목록에 추가
+        my_num_list.append(input_num)
     
 # 목록에 잘 들어갔는지 test
 print(my_num_list)
