694065313ae523858d8e202e6eaaae6f255a20bc
ooooo5h <ooooo5h91@gmail.com>
2021-12-23 14:48:31 +0900

    사용자가 입력한 숫자를 3칸짜리 목록으로 분해했다.

--- 03_repeat_test_hard_08.py
+++ 03_repeat_test_hard_08.py
@@ -9,3 +9,14 @@ cpu_numbers = random.sample( range (1, 10), 3 )
 
 # 테스트 : 출제되는 숫자들이 뭔지 확인해보자
 print(cpu_numbers)
+
+# 정답을 맞출 때 까지 계속 반복 입력
+# 언제 맞출지 모르니까 while
+while True:
+    input_num = int( input('3자리 숫자 입력 : '))
+    
+    # 숫자를 3자리로 나눠서 저장해 줄 목록을 생성
+    # 456 들어오면 => [4,5,6] => [100의 자리, 10의 자리, 1의 자리]
+    
+    user_numbers = [input_num // 100 , input_num // 10 % 10, input_num % 10]
+    print(user_numbers)
