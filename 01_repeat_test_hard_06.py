e9e7bbfb861d0e0528a36005b98fc498fdc88569
ooooo5h <ooooo5h91@gmail.com>
2021-12-23 09:46:25 +0900

    필요 변수 셋팅

--- 01_repeat_test_hard_06.py
+++ 01_repeat_test_hard_06.py
@@ -2,6 +2,14 @@
 # ex) 두 개의 정수 입력
 # ex) 최소공배수를 저장해 둘 변수
 
+num1 = int(input('첫 숫자 입력 : '))
+num2 = int(input('두번째 숫자 입력 : '))
+
+# 최소공배수 (least common multiple)
+lcm = 0
+
+# 최대공약수 (greatest common factor)
+gcf = 0
 
 ### 2 : 결과변수에 답을 저장하기 위한 로직
 # ex) for / while 등 반복, if / elif / else 조건
